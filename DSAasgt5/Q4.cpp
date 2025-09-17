77fe7bd85ba511082c6c031e37bb6ba60ffd824a
xan-antx <anant.agrawal9906@gmail.com>
2025-09-17 22:39:37 +0530

    Create Q4.cpp

---
+++ DSAasgt5/Q4.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int val;
+    Node* link;
+};
+
+Node* start = nullptr;
+
+// Add node to end
+void addNode(int x) {
+    Node* fresh = new Node;
+    fresh->val = x;
+    fresh->link = nullptr;
+
+    if (start == nullptr) {
+        start = fresh;
+        return;
+    }
+    Node* walk = start;
+    while (walk->link) {
+        walk = walk->link;
+    }
+    walk->link = fresh;
+}
+
+// Reverse the linked list
+Node* invert(Node* headRef) {
+    Node* prev = nullptr;
+    Node* curr = headRef;
+    while (curr) {
+        Node* nxt = curr->link;
+        curr->link = prev;
+        prev = curr;
+        curr = nxt;
+    }
+    return prev;
+}
+
+// Print list
+void show(Node* ptr) {
+    while (ptr) {
+        cout << ptr->val << " -> ";
+        ptr = ptr->link;
+    }
+    cout << "NULL\n";
+}
+
+int main() {
+    int n, x;
+    cout << "How many nodes? ";
+    cin >> n;
+    cout << "Enter values: ";
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        addNode(x);
+    }
+
+    start = invert(start);
+
+    cout << "Reversed list: ";
+    show(start);
+
+    return 0;
+}
